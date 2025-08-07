9950bdc469d765b324fa962eccc4ca0ec522de98
sun-wenqiang <284836621@qq.com>
2025-08-07 22:50:37 +0800

    A exaple for QT App

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+// test.cpp
+#include <QApplication>
+#include <QPushButton>
+
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+    QPushButton button("Hello Qt!");
+    button.show();
+    return app.exec();
+}
