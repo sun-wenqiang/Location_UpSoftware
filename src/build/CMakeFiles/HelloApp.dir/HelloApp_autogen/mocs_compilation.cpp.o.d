7c9b2014ae15615a3056116bf9317868263e504a
sun-wenqiang <284836621@qq.com>
2025-08-07 23:21:14 +0800

    Complete project structure

--- src/build/CMakeFiles/HelloApp.dir/HelloApp_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/HelloApp.dir/HelloApp_autogen/mocs_compilation.cpp.o: \
- /home/sun/code/Location_UpSoftware/src/build/HelloApp_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
